7cf80a7258705b515f8ac64dd70cccbd5f80cd9a
Mirzorasul Danierov <md@netinventors.de>
2023-10-27 23:42:32 +0200

    Refactoring | Changelog

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,7 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Showdown from "@/modules/editor/showdown-editor/Showdown.vue";
 
-const routes = [
-    {
-        path: "/",
-        component: Showdown
-    }
-];
+const routes = [];
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
